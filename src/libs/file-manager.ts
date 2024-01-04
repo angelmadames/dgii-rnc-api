19866b0d75ac6dc7ef83636a673105808e4ccb54
Angel M. Adames <angelmadames@gmail.com>
2024-01-03 22:13:50 -0400

    chore: Improve LOB principle on file-manager and rnc-parser files

--- src/libs/file-manager.ts
+++ src/libs/file-manager.ts
@@ -1,6 +1,7 @@
 import { confirm } from '@inquirer/prompts';
 import axios from 'axios';
-import fs from 'node:fs';
+import * as fs from 'node:fs';
+import * as readline from 'node:readline';
 
 class FileManager {
   isFile(path: string): boolean {
@@ -49,6 +50,25 @@ class FileManager {
     } catch (error) {
       throw new Error(`❌ Failed to download file. \nMessage: ${error}`);
     }
+  }
+
+  readFile(path: string, encoding: BufferEncoding = 'utf8') {
+    return fs.readFileSync(path, encoding);
+  }
+
+  readLines(
+    path: string,
+    enconding: BufferEncoding = 'utf8',
+    forEachLine: any,
+    onClose: any,
+  ) {
+    const rl = readline.createInterface({
+      input: fs.createReadStream(path, { encoding: enconding }),
+      crlfDelay: Infinity,
+    });
+
+    rl.on('line', forEachLine);
+    rl.on('close', onClose);
   }
 }
 
--- src/scripts/parse-rnc-file.ts
+++ src/scripts/parse-rnc-file.ts
@@ -1,36 +1,17 @@
-import type { RNC } from '../database/schema.ts';
-import FileParser from '../libs/file-parser';
+import FileManager from '../libs/file-manager.ts';
+import { RNCLineParser } from '../libs/rnc-parser.ts';
 
-const UNZIPPED_FILE_PATH = './TMP/DGII_RNC.TXT';
+const RNCCollectionFile =
+  process.env.UNZIPPED_FILE_PATH || './TMP/DGII_RNC.TXT';
 
-const content = FileParser.readFile(UNZIPPED_FILE_PATH, 'latin1');
-const csvContent = content.replace(/\|/g, ',').split('\n');
-const nonEmptyCsvContent = csvContent
-  .filter((line) => line.trim() !== '')
-  .join('\n');
-
-FileParser.readLine(
-  UNZIPPED_FILE_PATH,
+FileManager.readLines(
+  RNCCollectionFile,
   'latin1',
   (line: string) => {
-    const parsedLine = line
-      .split('|')
-      .map((field: string) => field.replace(/\s{2,}/g, ' '));
-    console.log(parsedLine);
-    const rncRecord: RNC = {
-      id: parsedLine[0],
-      name: parsedLine[1],
-      commercialName: parsedLine[2],
-      description: parsedLine[3],
-      address: `${parsedLine[4]} ${parsedLine[5]} ${parsedLine[6]}`,
-      phone: parsedLine[7],
-      creationDate: parsedLine[8],
-      status: parsedLine[9],
-      paymentSystem: parsedLine[10]
-    }
-    console.log(rncRecord);
+    const rnc = RNCLineParser(line);
+    console.log(rnc);
   },
   () => {
-    console.log('Done reading DGII RNC file.');
+    console.log('Done reading and parsing DGII RNC file.');
   },
 );
